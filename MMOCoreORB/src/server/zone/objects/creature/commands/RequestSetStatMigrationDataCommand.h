916637870eb901837fdc026d2b9ce403060459dc
Whiskey <RobinTh11@googlemail.com>
2023-11-07 00:58:30 +0100

    Update RequestSetStatMigrationDataCommand.h

--- MMOCoreORB/src/server/zone/objects/creature/commands/RequestSetStatMigrationDataCommand.h
+++ MMOCoreORB/src/server/zone/objects/creature/commands/RequestSetStatMigrationDataCommand.h
@@ -6,12 +6,12 @@
 #define REQUESTSETSTATMIGRATIONDATACOMMAND_H_
 
 #include "server/zone/objects/creature/CreatureObject.h"
-#include "server/zone/objects/player/sessions/MigrateStatsSession.idl"
+//#include "server/zone/objects/player/sessions/MigrateStatsSession.idl"
 #include "server/zone/managers/player/creation/PlayerCreationManager.h"
 
 class RequestSetStatMigrationDataCommand : public QueueCommand {
 public:
-
+/*
 	RequestSetStatMigrationDataCommand(const String& name, ZoneProcessServer* server)
 		: QueueCommand(name, server) {
 
@@ -91,7 +91,7 @@ public:
 
 		return SUCCESS;
 	}
-
+*/
 };
 
 #endif //REQUESTSETSTATMIGRATIONDATACOMMAND_H_
