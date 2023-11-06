873a9fea3cca94a422fa65a07a556c955ec8f237
Whiskey <RobinTh11@googlemail.com>
2023-11-06 23:07:18 +0100

    Update BaseClientProxy.cpp

--- MMOCoreORB/utils/engine3/MMOEngine/src/engine/service/proto/BaseClientProxy.cpp
+++ MMOCoreORB/utils/engine3/MMOEngine/src/engine/service/proto/BaseClientProxy.cpp
@@ -23,6 +23,61 @@ BaseClientProxy::BaseClientProxy(Socket* sock, SocketAddress& addr) : BaseClient
    	//setFileLogger("log/" + addr.getFullPrintableIPAddress());
 }
 
+bool BaseClient::updateNetStatus(uint16 recievedTick) {
+	lock();
+
+	try {
+		if (!isAvailable()) {
+			unlock();
+			return false;
+		}
+
+		uint16 hostByte = htons(recievedTick);
+
+		if (lastRecievedNetStatusTick != 0) {
+			uint16 clientDelta = hostByte - lastRecievedNetStatusTick;
+			uint16 serverDelta = lastNetStatusTimeStamp.miliDifference();
+
+			/*StringBuffer msg;
+			msg << "recievedTick: " << hostByte << " clientDelta:" << clientDelta << " serverDelta:" << serverDelta;
+			debug(msg, true);*/
+			/*
+						if (clientDelta > serverDelta) {
+							uint16 difference = clientDelta - serverDelta;
+
+							if ((difference > 200) && (++erroneusTicks > 10)) {
+								disconnect("client clock desync", false);
+
+								unlock();
+
+								return false;
+							}
+						} else
+							erroneusTicks = 0;
+
+							*/
+		}
+
+		lastNetStatusTimeStamp.updateToCurrentTime();
+		lastRecievedNetStatusTick = hostByte;
+
+#ifdef TRACE_CLIENTS
+		debug("setting net status");
+#endif
+
+		netcheckupEvent->rescheduleInIoScheduler(NETSTATUSCHECKUP_TIMEOUT);
+	} catch (Exception& e) {
+		e.printStackTrace();
+		disconnect("Exception on updateNetStatus()", false);
+	} catch (...) {
+		disconnect("unreported exception on updateNetStatus()", false);
+	}
+
+	unlock();
+
+	return true;
+}
+
 void BaseClientProxy::init(DatagramServiceThread* serv) {
 	crcSeed = System::random();
 
