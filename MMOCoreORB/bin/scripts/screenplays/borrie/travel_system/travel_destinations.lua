716f8a009ca837f0ec66fa945c859b1dd3726068
Whiskey <RobinTh11@googlemail.com>
2023-11-12 18:43:23 +0100

    Update travel_destinations.lua

--- MMOCoreORB/bin/scripts/screenplays/borrie/travel_system/travel_destinations.lua
+++ MMOCoreORB/bin/scripts/screenplays/borrie/travel_system/travel_destinations.lua
@@ -3,73 +3,73 @@
 travel_destinations = {
 
 	--Carida
-	--{
-	--	name = "Carida",
-	--	zone = "rp_carida",
-	--	destinations = {
-	--		{"Imperial Academy", "rp_carida", 8, 320, 2916, 0, "empire"},
-	--	}
-	--},
+	{
+		name = "Carida",
+		zone = "rp_carida",
+		destinations = {
+			{"Imperial Academy", "rp_carida", 8, 320, 2916, 0, "empire"},
+		}
+	},
 
 	--Dantooine
-	--{ 
-	--	name = "Dantooine",
-	--	zone = "rp_dantooine",
-	--	destinations = {
-	--		{"Damarise Outpost", "rp_dantooine", 1368, 4, 3403, 0, "public"},
-	--	}
+	{ 
+		name = "Dantooine",
+		zone = "rp_dantooine",
+		destinations = {
+			{"Damarise Outpost", "rp_dantooine", 1368, 4, 3403, 0, "public"},
+		}
 	
-	--},
+	},
 	
 	--Dathomir
-	--{ 
-	--	name = "Dathomir",
-	--	zone = "rp_dathomir",
-	--	destinations = {
-	--		{"Prison Facility", "rp_dathomir", 0, 0, 0, 0, "empire"}
-	--	}
+	{ 
+		name = "Dathomir",
+		zone = "rp_dathomir",
+		destinations = {
+			{"Prison Facility", "rp_dathomir", -6248.52, 120, 977.714, 0, "empire"}
+		}
 	
-	--},
+	},
 	
 	--Ord Mantell
-	--{ 
-	--	name = "Ord Mantell",
-	--	zone = "rp_space",
-	--	destinations = {
-	--		{"Tansarii Point Station", "rp_space", 52.3, 0.9, 27.8, 808251, "public"},
-	--	}	
-	--},
+	{ 
+		name = "Ord Mantell",
+		zone = "rp_space",
+		destinations = {
+			{"Tansarii Point Station", "rp_space", 52.3, 0.9, 27.8, 808251, "public"},
+		}	
+	},
 	
 	--Deep Space
-	--{ 
-	--	name = "Deep Space",
-	--	zone = "rp_space",
-	--	destinations = {
-	--		{"Corellian Corvette", "rp_space", -34.8, 0, 0.1, 808180, "newrepublic"},
-	--		{"Star Destroyer", "rp_space", 10, 173.8, 42.2, 808038, "empire"},
-	--	}	
-	--},
+	{ 
+		name = "Deep Space",
+		zone = "rp_space",
+		destinations = {
+			{"Corellian Corvette", "rp_space", -34.8, 0, 0.1, 808180, "newrepublic"},
+			{"Star Destroyer", "rp_space", 10, 173.8, 42.2, 808038, "empire"},
+		}	
+	},
 	
 	--Rori
-	--{ 
-	--	name = "Rori",
-	--	zone = "rp_rori",
-	--	destinations = {
-	--		{"New Restuss", "rp_rori", -775, 83, 1554, 0, "public"},
-	--	}
+	{ 
+		name = "Rori",
+		zone = "rp_rori",
+		destinations = {
+			{"New Restuss", "rp_rori", -775, 83, 1554, 0, "public"},
+		}
 	
-	--},
+	},
 
 	--Sulon
-	--{ 
-	--	name = "Sulon",
-	--	zone = "rp_sulon",
-	--	destinations = {
-	--		{"Saltalo Station", "rp_sulon", 4346, 6.0, -5382, 0, "public"},
-	--		{"Barons Hed", "rp_sulon", 4346, 6.0, -5382, 0, "public"},
-	--	}
+	{ 
+		name = "Sulon",
+		zone = "rp_sulon",
+		destinations = {
+			{"Saltalo Station", "rp_sulon", 4346, 6.0, -5382, 0, "public"},
+			{"Barons Hed", "rp_sulon", -6401, 6.0, 2202, 0, "public"},
+		}
 	
-	--},
+	},
 	
 	--Tatooine
 	{ 
@@ -78,7 +78,7 @@ travel_destinations = {
 		destinations = {
 			{"Anchorhead", "rp_tatooine", -560, 0.0, -1770, 0, "public"},
 			{"Mos Eisley", "rp_tatooine", 1150, 0.0, 100, 0, "public"},
-			{"Mos Eisley Garrison", "rp_tatooine", 1150, 0.0, 100, 0, "faction"},
+			{"Mos Eisley Garrison", "rp_tatooine", 597, 0.0, 5, 0, "faction"},
 		}	
 	},
 }
